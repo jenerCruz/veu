773fd6e672d40cc26e9d6c2b24957eb52a35cf78
Eduardo cruz <pamecoaxaca.telcel@gmail.com>
2025-11-30 17:21:30 -0600

    Create sales.vue

---
+++ src/views/apps/sales.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold mb-4">Productos</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id" class="mb-2 p-2 border rounded">
+        {{ product.name }} - ${{ product.price }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { useSales } from '@/composables/useSales.js';
+const { products } = useSales();
+</script>
