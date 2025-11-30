47d3b8601a7c5700dd459e9db7ff709e8dff94df
Eduardo cruz <pamecoaxaca.telcel@gmail.com>
2025-11-30 17:21:06 -0600

    Create TimeTracker.vue

---
+++ src/views/apps/TimeTracker.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold mb-4">Time Tracker</h1>
+    <ul>
+      <li v-for="entry in entries" :key="entry.id" class="mb-2 p-2 border rounded">
+        {{ entry.task }} - {{ entry.hours }} horas
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { useTimeTracker } from '@/composables/useTimeTracker.js';
+const { entries } = useTimeTracker();
+</script>
