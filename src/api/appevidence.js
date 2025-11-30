882ba322d746714fb28d2d836f4ec2868ce5d02c
Eduardo cruz <pamecoaxaca.telcel@gmail.com>
2025-11-30 17:17:19 -0600

    Create appevidence.js

---
+++ src/api/appevidence.js
@@ -0,0 +1,7 @@
+export const fetchFiles = async () => {
+  // Simulación de llamada a API
+  return [
+    { id: 1, name: "archivo1.pdf", size: "2MB" },
+    { id: 2, name: "archivo2.pdf", size: "1MB" },
+  ];
+};
